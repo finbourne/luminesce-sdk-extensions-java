8043ecf263401aed87291789c715dc6f1549f558
Concourse <concourse@finbourne.com>
2022-05-03 15:56:14 +0000

    Update SDK Extensions 0.6.0 - autogenerated by MustacheGenerator

---
+++ sdk/src/test/unit/java/com/finbourne/luminesce/extensions/CredentialsSource.java
@@ -0,0 +1,8 @@
+package com.finbourne.luminesce.extensions;
+
+/**
+ * Filename containing luminesce API credentials
+ */
+public class CredentialsSource {
+    public static final String credentialsFile = "secrets.json";
+}
