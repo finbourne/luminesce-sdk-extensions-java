33dc81f3e20aa0d7f171410e3a0b23a23554d990
matthew-boardman <matthew.boardman@finbourne.com>
2022-05-03 17:19:39 +0100

    CTECH-1181 test creation

--- sdk/src/test/integration/java/com/finbourne/luminesce/extensions/ApiExceptionTests.java
+++ sdk/src/test/integration/java/com/finbourne/luminesce/extensions/ApiExceptionTests.java
@@ -2,47 +2,44 @@ package com.finbourne.luminesce.extensions;
 
 import com.finbourne.luminesce.ApiClient;
 import com.finbourne.luminesce.ApiException;
-// UNCOMMENT BELOW LINE AND IMPORT ONE OF THE APIS
-// import com.finbourne.luminesce.api.;
+import com.finbourne.luminesce.api.HistoricallyExecutedQueriesApi;
 import com.finbourne.luminesce.extensions.auth.FinbourneTokenException;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
 
-// UNCOMMENT TEST BELOW TO USE THE IMPORTED API. HERE WE ARE USING DRIVE FOLDERS API AS AN EXAMPLE
-// public class ApiExceptionTests {
+public class ApiExceptionTests {
+ @Test
+     public void thrown_exception_tostring_contains_requestid() throws ApiConfigurationException, FinbourneTokenException {
 
-//     @Test
-//     public void thrown_exception_tostring_contains_requestid() throws ApiConfigurationException, FinbourneTokenException {
+         ApiConfiguration apiConfiguration = new ApiConfigurationBuilder().build(CredentialsSource.credentialsFile);
+         ApiClient apiClient = new ApiClientBuilder().build(apiConfiguration);
 
-//         ApiConfiguration apiConfiguration = new ApiConfigurationBuilder().build(CredentialsSource.credentialsFile);
-//         ApiClient apiClient = new ApiClientBuilder().build(apiConfiguration);
+         HistoricallyExecutedQueriesApi historicallyExecutedQueriesApi = new HistoricallyExecutedQueriesApi(apiClient);
 
-//         FoldersApi foldersApi = new FoldersApi(apiClient);
+         try {
+             historicallyExecutedQueriesApi.getProgressOfHistory("doesntExist");
+         }
+         catch (ApiException e) {
 
-//         try {
-//             foldersApi.getFolder("doesntExist");
-//         }
-//         catch (ApiException e) {
+             String message = e.toString();
 
-//             String message = e.toString();
+             assertNotNull("Null exception message", message);
 
-//             assertNotNull("Null exception message", message);
+             String[] parts = message.split("\\r?\\n");
 
-//             String[] parts = message.split("\\r?\\n");
+             assertThat(parts.length, is(greaterThanOrEqualTo(1)));
 
-//             assertThat(parts.length, is(greaterThanOrEqualTo(1)));
+             //  of the format 'LUSID request id = 000000000:AAAAAAA'
+             String[] idParts = parts[0].split(" = ");
 
-//             //  of the format 'LUSID request id = 000000000:AAAAAAA'
-//             String[] idParts = parts[0].split(" = ");
-
-//             assertThat("missing requestId", idParts.length, is(equalTo(2)));
-//         }
-//         catch (Exception e) {
-//             fail("Unexpected exception of type " + e.getClass());
-//         }
+             assertThat("missing requestId", idParts.length, is(equalTo(2)));
+         }
+         catch (Exception e) {
+             fail("Unexpected exception of type " + e.getClass());
+         }
 
 
-//     }
-// }
+     }
+}
