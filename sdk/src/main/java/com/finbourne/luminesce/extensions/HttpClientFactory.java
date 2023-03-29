8fa37a0447c71eb2a63b32fd664cf2047678b25e
Robert Byrne <robert.byrne@finbourne.com>
2023-03-29 10:53:24 +0100

    WIP: Add 429 retries and bump version to 0.79.1
    Add an interceptor to the http client which will retry on a 429 code.
    
    This isn't finished, just put in so we could get something else working:
    
    * the number of retries is hardcoded to 3
    * it doesn't respect the http header telling you how much to back off
    * no tests
    * Doesn't match the C# SDK

--- sdk/src/main/java/com/finbourne/luminesce/extensions/HttpClientFactory.java
+++ sdk/src/main/java/com/finbourne/luminesce/extensions/HttpClientFactory.java
@@ -2,10 +2,16 @@ package com.finbourne.luminesce.extensions;
 
 import okhttp3.Credentials;
 import okhttp3.OkHttpClient;
+import okhttp3.OkHttpClient.Builder;
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
 
 import java.net.InetSocketAddress;
 import java.net.Proxy;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
+import java.lang.InterruptedException;
 
 /**
 * Builds http client to communicate to luminesce API instances.
@@ -34,32 +40,42 @@ public class HttpClientFactory {
      * @return n client for http calls to luminesce API
      */
     public OkHttpClient build(ApiConfiguration apiConfiguration, int readTimeout, int writeTimeout){
-        final OkHttpClient httpClient;
-
+        Builder builder = new OkHttpClient.Builder()
+            .readTimeout(readTimeout, TimeUnit.SECONDS)
+            .writeTimeout(writeTimeout, TimeUnit.SECONDS)
+        ;
         //  use a proxy if given
         if (apiConfiguration.getProxyAddress() != null) {
-
-            InetSocketAddress proxy = new InetSocketAddress(apiConfiguration.getProxyAddress(), apiConfiguration.getProxyPort());
-
-            httpClient = new OkHttpClient.Builder()
-                .readTimeout(readTimeout, TimeUnit.SECONDS)
-                .writeTimeout(writeTimeout, TimeUnit.SECONDS)
-                .proxy(new Proxy(Proxy.Type.HTTP, proxy))
+            InetSocketAddress address = new InetSocketAddress(apiConfiguration.getProxyAddress(), apiConfiguration.getProxyPort());
+            builder
+                .proxy(new Proxy(Proxy.Type.HTTP, address))
                 .proxyAuthenticator((route, response) -> {
                     String credential = Credentials.basic(apiConfiguration.getProxyUsername(), apiConfiguration.getProxyPassword());
                     return response.request().newBuilder()
                             .header("Proxy-Authorization", credential)
                             .build();
-            })
-            .build();
+                })
+            ;
         }
-        else {
-            httpClient = new OkHttpClient.Builder()
-                .readTimeout(readTimeout, TimeUnit.SECONDS)
-                .writeTimeout(writeTimeout, TimeUnit.SECONDS)
-                .build();
-        }
-        return httpClient;
+        builder.addInterceptor(new Interceptor() {
+            @Override
+            public Response intercept(Chain chain) throws IOException {
+                Request request = chain.request();
+                Response response = chain.proceed(request);
+                int tryCount = 0;
+                try {
+                    while (response.code() == 429 && tryCount < 3) {
+                        Thread.sleep(1000);  // should take this delay from the header
+                        tryCount++;
+                        response.close();
+                        response = chain.proceed(request);
+                    }
+                } catch(InterruptedException e) {}
+                // otherwise just pass the original response on
+                return response;
+            }
+        });
+        return builder.build();
     }
 
 }
